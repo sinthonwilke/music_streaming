bca6d461709f5a1d7e0b03e28625992f0869c18d
Sinthon Wilke <sinthonwilke@gmail.com>
2022-11-27 22:21:56 +0700

    update

--- Music Streaming/demo/demo/src/main/resources/static/js/index.js
+++ Music Streaming/demo/demo/src/main/resources/static/js/index.js
@@ -2,7 +2,6 @@ window.onload = function() {
     home()
   };
 
-
 function home() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
@@ -21,6 +20,39 @@ function search() {
     xhttp.send();
 }
 
+    function searchResult() {
+
+        var delayInMilliseconds = 0;
+        setTimeout(function() {
+            var str = document.getElementById("searchBox").value;
+            var url = "/search_results=" + str;
+
+            showMusic(load(url))
+
+        }, delayInMilliseconds);
+
+    }
+
+    function load(url) {
+        const xhttp = new XMLHttpRequest();
+        var res;
+        xhttp.onload = function() {
+            res = this.response;
+        }
+        xhttp.open("GET", url, false);
+        xhttp.send();
+        return res;
+    }
+
+    function showMusic(musics) {
+        var music = JSON.parse(musics);
+        var out = "";
+        for(var i = 0; i < music.length; i++) {
+            out += '<button type="button" onclick="playMusic(music[i].id)">' + music[i].artist.name + " - " +music[i].name + '</button>';
+        }
+        document.getElementById("result").innerHTML = out;
+    }
+
 function library() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
@@ -37,4 +69,11 @@ function account() {
     }
     xhttp.open("GET", "/account");
     xhttp.send();
+}
+
+function playMusic(id) {
+    var audio = document.getElementById("audio");
+    audio.src="assets/musics/" + id + ".wav";
+    audio.volume = 0.25;
+    audio.play();
 }
